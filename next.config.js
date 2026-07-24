0a4ccd19686ff5a7c09e62ca0aafe6c55f43283d
AlanAkmil <alansubagyo058@gmail.com>
2026-07-24 07:59:28 +0700

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // manifest.json, sw.js, icon-*.png, data.js, script.js, admin.html
-  // otomatis ke-serve dari folder public/ tanpa perlu rewrite tambahan.
-};
-
-module.exports = nextConfig;
